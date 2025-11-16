2a3ab6ad045ec6a707ba82fe8f29264102864e72
PeakMentalityCoach <admin@peakmentalitycoaching.com>
2025-11-16 23:16:55 +0100

    Add GameTab component for match interface

---
+++ src/components/src/utils/src/components/src/components/GameTab.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function GameTab() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold mb-4">Match Mode</h2>
+      <p>This is the start of your live match interface.</p>
+      <p>We will expand this next.</p>
+    </div>
+  );
+}
